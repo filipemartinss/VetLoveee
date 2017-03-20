54c16ef71dd1df6b878710bcb5890e46cdcb45d9
Filipe <Filipe@FIlipeMartins>
2017-03-20 16:58:37 +0000

    parte 1

---
+++ src/aplicacao/App.java
@@ -0,0 +1,11 @@
+package aplicacao;
+
+public class App {
+	public  static  void  main ( String [] args ) {
+		// TODO Soquete de método gerado automaticamente
+		System.out.println("Ola turma!");
+	}
+	
+	
+	
+}
